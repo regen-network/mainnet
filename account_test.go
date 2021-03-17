c3dc0f84170f0cad42650822fc9b1fa4cb56bf95
Aaron Craelius <aaronc@users.noreply.github.com>
2021-03-17 17:04:06 -0400

    WIP

--- account_test.go
+++ account_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 	"time"
 
+	vesting "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	auth "github.com/cosmos/cosmos-sdk/x/auth/types"
@@ -25,10 +27,16 @@ func TestRegenToCoins(t *testing.T) {
 
 func TestToCosmosAccount(t *testing.T) {
 	addr0 := sdk.AccAddress("abcdefg012345")
+	addr1 := sdk.AccAddress("012345abcdefg")
+	five, _, err := apd.NewFromString("5")
+	require.NoError(t, err)
 	ten, _, err := apd.NewFromString("10")
 	require.NoError(t, err)
 
 	time0, err := time.Parse(time.RFC3339, "2021-05-21T00:00:00Z")
+	require.NoError(t, err)
+
+	time1, err := time.Parse(time.RFC3339, "2021-06-21T00:00:00Z")
 	require.NoError(t, err)
 
 	tests := []struct {
@@ -51,6 +59,50 @@ func TestToCosmosAccount(t *testing.T) {
 			},
 			&bank.Balance{
 				Address: addr0.String(),
+				Coins:   sdk.NewCoins(sdk.NewInt64Coin(URegenDenom, 10000000)),
+			},
+			false,
+		},
+		{
+			"two distributions",
+			Account{
+				Address:    addr1,
+				TotalRegen: *ten,
+				StartTime:  time0,
+				EndTime:    time1,
+				Distributions: []Distribution{
+					{
+						Time:  time0,
+						Regen: *five,
+					},
+					{
+						Time:  time1,
+						Regen: *five,
+					},
+				},
+			},
+			&vesting.PeriodicVestingAccount{
+				BaseVestingAccount: &vesting.BaseVestingAccount{
+					BaseAccount: &auth.BaseAccount{
+						Address: addr1.String(),
+					},
+					OriginalVesting: sdk.NewCoins(sdk.NewInt64Coin(URegenDenom, 10000000)),
+					EndTime:         time1.Unix(),
+				},
+				StartTime: time0.Unix(),
+				VestingPeriods: []vesting.Period{
+					{
+						Length: 0,
+						Amount: sdk.NewCoins(sdk.NewInt64Coin(URegenDenom, 5000000)),
+					},
+					{
+						Length: 0,
+						Amount: sdk.NewCoins(sdk.NewInt64Coin(URegenDenom, 5000000)),
+					},
+				},
+			},
+			&bank.Balance{
+				Address: addr1.String(),
 				Coins:   sdk.NewCoins(sdk.NewInt64Coin(URegenDenom, 10000000)),
 			},
 			false,
