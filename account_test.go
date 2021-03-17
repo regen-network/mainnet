353fad0370c1e4d4e3dd802f188baf542e4cc6a5
Aaron Craelius <aaronc@users.noreply.github.com>
2021-03-17 16:52:17 -0400

    Adding tests

--- account_test.go
+++ account_test.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"reflect"
 	"testing"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	auth "github.com/cosmos/cosmos-sdk/x/auth/types"
+	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
 
 	"github.com/cockroachdb/apd/v2"
 	"github.com/stretchr/testify/require"
@@ -17,4 +24,51 @@ func TestRegenToCoins(t *testing.T) {
 }
 
 func TestToCosmosAccount(t *testing.T) {
+	addr0 := sdk.AccAddress("abcdefg012345")
+	ten, _, err := apd.NewFromString("10")
+	require.NoError(t, err)
+
+	time0, err := time.Parse(time.RFC3339, "2021-05-21T00:00:00Z")
+	require.NoError(t, err)
+
+	tests := []struct {
+		name    string
+		acc     Account
+		want    auth.AccountI
+		want1   *bank.Balance
+		wantErr bool
+	}{
+		{
+			"base account",
+			Account{
+				Address:    addr0,
+				TotalRegen: *ten,
+				StartTime:  time0,
+				EndTime:    time0,
+			},
+			&auth.BaseAccount{
+				Address: addr0.String(),
+			},
+			&bank.Balance{
+				Address: addr0.String(),
+				Coins:   sdk.NewCoins(sdk.NewInt64Coin(URegenDenom, 10000000)),
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := ToCosmosAccount(tt.acc)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ToCosmosAccount() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToCosmosAccount() got AccountI = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("ToCosmosAccount() got Balance = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
 }
