6bd6c7b3d1e8703fe59a199b267665f70e0ca833
Aaron Craelius <aaronc@users.noreply.github.com>
2021-03-23 21:10:44 -0400

    Add auditing output

---
+++ audit_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cockroachdb/apd/v2"
+)
+
+func ExamplePrintAccountAudit() {
+	addr0, err := sdk.AccAddressFromBech32("regen1ktvsz07ca4amg5cja68pph3t65lj4wdulrz464")
+	if err != nil {
+		panic(err)
+	}
+	addr1, err := sdk.AccAddressFromBech32("regen16ll5l3zdu086ug96cau00k3rllqg9eeyz7ss7t")
+	if err != nil {
+		panic(err)
+	}
+	genesisTime, err := time.Parse(time.RFC3339, "2021-04-08T16:00:00Z")
+	if err != nil {
+		panic(err)
+	}
+	t0, err := time.Parse(time.RFC3339, "2021-05-03T16:00:00Z")
+	if err != nil {
+		panic(err)
+	}
+	t1, err := time.Parse(time.RFC3339, "2021-05-08T16:00:00Z")
+	if err != nil {
+		panic(err)
+	}
+	five, _, _ := apd.NewFromString("5")
+	ten, _, _ := apd.NewFromString("10")
+	fifteen, _, _ := apd.NewFromString("15")
+
+	PrintAccountAudit([]Account{
+		{
+			Address:    addr0,
+			TotalRegen: *ten,
+			Distributions: []Distribution{
+				{
+					Time:  genesisTime,
+					Regen: *five,
+				},
+				{
+					Time:  t1,
+					Regen: *five,
+				},
+			},
+		},
+		{
+			Address:    addr1,
+			TotalRegen: *fifteen,
+			Distributions: []Distribution{
+				{
+					Time:  t0,
+					Regen: *ten,
+				},
+				{
+					Time:  t1,
+					Regen: *five,
+				},
+			},
+		},
+	}, genesisTime, os.Stdout)
+	//Output:
+	//regen16ll5l3zdu086ug96cau00k3rllqg9eeyz7ss7t	15	2
+	//	10	2021-05-03 16:00:00
+	//	5	2021-05-08 16:00:00
+	//regen1ktvsz07ca4amg5cja68pph3t65lj4wdulrz464	10	2
+	//	5	MAINNET
+	//	5	2021-05-08 16:00:00
+}
